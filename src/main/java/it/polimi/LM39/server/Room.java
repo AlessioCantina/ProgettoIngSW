057689b3377311883f33252364b4fc5332c78348
SimoneCrippa <simone3.crippa@mail.polimi.it>
2017-05-28 17:04:23 +0200

    room manager with some useful getters

--- src/main/java/it/polimi/LM39/server/Room.java
+++ src/main/java/it/polimi/LM39/server/Room.java
@@ -6,25 +6,30 @@ import java.util.*;
  * 
  */
 public class Room {
-
+	public static final Integer ROOM_CAPACITY = 4;
+	private static Integer roomCounter;
+	private ArrayList<InterfacePlayer> players;
     /**
      * Default constructor
      */
     public Room() {
+    	players = new ArrayList<InterfacePlayer>();
+    	roomCounter++;
+    }
+    
+    public Integer getConnectedPlayers(){
+    	return this.players.size();
+    }
+    public Integer getNumberOfRooms(){
+    	return roomCounter;
     }
 
-    /**
-     * 
-     */
-    private InterfacePlayer players;
-
-
-
-    /**
-     * @return
-     */
-    public void connect() {
-
+    public void connect(InterfacePlayer player) {
+    	if(this.getConnectedPlayers() == ROOM_CAPACITY || this.getConnectedPlayers() == 0){
+    		Room room = new Room();
+    		room.players.add(player);
+    	}
+    	else
+    		this.players.add(player);
     }
-
 }
