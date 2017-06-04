2b4134eef97cfad94c716e3d10fe95cc40dab58a
SimoneCrippa <simone3.crippa@mail.polimi.it>
2017-06-04 15:24:38 +0200

    modified servantsmalus effect

--- src/main/java/it/polimi/LM39/model/excommunicationpermanenteffect/ServantsMalus.java
+++ src/main/java/it/polimi/LM39/model/excommunicationpermanenteffect/ServantsMalus.java
@@ -6,9 +6,10 @@ package it.polimi.LM39.model.excommunicationpermanenteffect;
 public class ServantsMalus extends ExcommunicationPermanentEffect {
 
     /**
-     * You have to spend 2 servants to increase your action value by 1
+     * You have to spend *servantsQty* servants to increase your action value by 1
      */
     public ServantsMalus() {
     }
 
+    public Integer servantsQty;
 }
