2cab166aae9e3b27e4956f8b056b0e737cd7b8db
Simone Crippa <simone3.crippa@mail.polimi.it>
2017-06-07 15:39:15 +0200

    added methods to CLI

--- src/main/java/it/polimi/LM39/client/CLI.java
+++ src/main/java/it/polimi/LM39/client/CLI.java
@@ -1,8 +1,14 @@
 package it.polimi.LM39.client;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import it.polimi.LM39.model.Building;
 import it.polimi.LM39.model.FamilyMember;
 import it.polimi.LM39.model.FamilyMembersLocation;
 import it.polimi.LM39.model.MainBoard;
+import it.polimi.LM39.model.PersonalBoard;
+import it.polimi.LM39.server.NetworkPlayer;
 
 /**
  * 
@@ -19,6 +25,8 @@ public class CLI extends UserInterface {
      * 
      */
     public void showNetworkMenu(){}
+    
+    
 
 	@Override
 	public void printMainBoard(MainBoard mainBoard) {
@@ -61,6 +69,73 @@ public class CLI extends UserInterface {
 		,this.getFamilyMemberColor(familyOnTowers[3][2]),this.getFamilyMemberColor(familyOnTowers[3][3]));
 		System.out.printf("╚════════════════════╩════════╩════════════════════╩════════╩════════════════════╩════════╩════════════════════╩════════╝\n");
 		
+	}
+	
+	public void printMarket(MainBoard mainBoard){
+		int marketSize = 4;
+		System.out.printf("╔════════════════════╦═══════════════════╗\n");
+		System.out.printf("║%20s║%20s║\n");
+		System.out.printf("║%20s║%20s║\n");
+		System.out.printf("║%20s║%20s║\n");
+		System.out.printf("║%20s║%20s║\n");
+		System.out.printf("╚════════════════════╩═══════════════════╝\n");
+		if(marketSize == 4){
+			System.out.printf("╔════════════════════╦═══════════════════╗\n");
+			System.out.printf("║%20s║%20s║\n");
+			System.out.printf("║%20s║%20s║\n");
+			System.out.printf("║%20s║%20s║\n");
+			System.out.printf("║%20s║%20s║\n");
+			System.out.printf("╚════════════════════╩═══════════════════╝\n");
+		}
+	}
+	public void printCouncilPalace(MainBoard mainBoard){
+		FamilyMembersLocation location = mainBoard.getFamilyMembersLocation();
+		ArrayList<FamilyMember> councilPalace = location.getFamilyMembersAtTheCouncilPalace();
+		int membersToPrint = councilPalace.size();
+		int i = 0;
+		System.out.printf("╔════════════════════════════════════════╗\n");
+		while(membersToPrint != 0){
+			System.out.printf("║%20s║\n",councilPalace.get(i).playerColor);
+			System.out.printf("║%20s║\n",councilPalace.get(i).color);
+			i++;
+		}
+		System.out.printf("╚════════════════════════════════════════╝\n");
+	}
+	public void printPossesedCards(NetworkPlayer player, MainBoard mainBoard){
+		PersonalBoard board = player.personalBoard;
+		ArrayList<Integer> buildings = board.possessedBuildings;
+		ArrayList<Integer> territories = board.possessedTerritories;
+		ArrayList<Integer> ventures = board.possessedVentures;
+		ArrayList<Integer> characters = board.possessedCharacters;
+		ArrayList<String> leaders = board.possessedLeaders;
+		System.out.println("Possesed buildings:");
+		printCardType("Building",buildings);
+		System.out.println("Possesed territories:");
+		printCardType("Territory",territories);
+		System.out.println("Possesed ventures:");
+		printCardType("Venture",ventures);
+		System.out.println("Possesed characters:");
+		printCardType("Character",characters);
+		System.out.println("Possesed leaders:");
+		//printCardType("Leader",leaders);
+		
+	}
+	public void printCardType(String cardType, ArrayList<Integer> cardMap){
+		int i = 0;
+		while (cardMap.size() < i){
+			if(("Territory").equals(cardType))
+				System.out.println(MainBoard.territoryMap.get(cardMap.get(i)));
+			else if(("Building").equals(cardType))
+				System.out.println(MainBoard.buildingMap.get(cardMap.get(i)));
+			else if(("Venture").equals(cardType))
+				System.out.println(MainBoard.ventureMap.get(cardMap.get(i)));
+			else if(("Character").equals(cardType))
+				System.out.println(MainBoard.characterMap.get(cardMap.get(i)));
+			else if(("Leader").equals(cardType))
+				System.out.println(MainBoard.leaderMap.get(i));
+			i++;
+			System.out.println("\n");
+		}
 	}
 	public String getPlayerColor(FamilyMember familyOnTower){
 		if(familyOnTower == null)
