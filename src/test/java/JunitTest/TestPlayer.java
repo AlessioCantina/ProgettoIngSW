c42f1ec07d4121fd61267f550cde6413efa2e65b
SimoneCrippa <simone3.crippa@mail.polimi.it>
2017-07-02 09:53:35 +0200

    first test case

---
+++ src/test/java/JunitTest/TestPlayer.java
@@ -0,0 +1,21 @@
+package JunitTest;
+
+import it.polimi.LM39.model.MainBoard;
+import it.polimi.LM39.server.NetworkPlayer;
+
+public class TestPlayer extends NetworkPlayer{
+
+	@Override
+	public String sendMessage() {
+		return null;
+	}
+
+	@Override
+	public void setMessage(String message) {
+	}
+
+	@Override
+	public void setMessage(MainBoard mainBoard) {
+	}
+
+}
---
+++ src/test/java/JunitTest/TestupdateRankings.java
@@ -0,0 +1,41 @@
+package JunitTest;
+
+import org.junit.Before;
+
+import it.polimi.LM39.controller.GameHandler;
+import it.polimi.LM39.model.MainBoard;
+import it.polimi.LM39.model.PlayerPoints;
+import junit.framework.TestCase;
+
+public class TestupdateRankings extends TestCase{
+	private TestPlayer testPlayer1 = new TestPlayer();
+	private MainBoard testMainBoard = new MainBoard();
+	private GameHandler testGame = new GameHandler();
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		testPlayer1.points = new PlayerPoints();
+		testPlayer1.points.setFaith(1);
+		testPlayer1.points.setVictory(1);
+		testPlayer1.points.setMilitary(1);
+		testPlayer1.playerColor = "black";
+		testGame.mainBoard = testMainBoard;
+	}
+
+	public void testRankings() {
+		int player1Victory = 1;
+		int player1Faith = 1;
+		int player1Military = 1;
+		int testResult;
+		testGame.updateRankings(testPlayer1);
+		testResult = testPlayer1.points.getFaith();
+		assertEquals(testResult,player1Faith);
+		testResult = testPlayer1.points.getVictory();
+		assertEquals(testResult,player1Victory);
+		testResult = testPlayer1.points.getMilitary();
+		assertEquals(testResult,player1Military);
+	}
+	
+}
